9e8edc504b6803e9ba6e41ecda65a37b3de18617
dahuin <s2001@e-mirim.hs.kr>
2022-08-19 10:35:38 +0900

    #1 제네릭

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -10,6 +10,13 @@ namespace CSClass2
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("김다흰⊙﹏⊙");
+            Wanted<int> wantedInt = new Wanted<int>(1234);
+            Wanted<double> wantdeDouble = new Wanted<double>(1234.12);
+            
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantdeDouble.Value);
         }
     }
 }
