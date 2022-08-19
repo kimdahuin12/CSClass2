a0f2ec2a57ecb86255adc3df8a0e4d22836cf0ac
dahuin <s2001@e-mirim.hs.kr>
2022-08-19 10:49:51 +0900

    #2 인덱서(Indexer)

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -13,10 +13,15 @@ namespace CSClass2
             Wanted<string> wantedString = new Wanted<string>("김다흰⊙﹏⊙");
             Wanted<int> wantedInt = new Wanted<int>(1234);
             Wanted<double> wantdeDouble = new Wanted<double>(1234.12);
-            
+
             Console.WriteLine(wantedString.Value);
             Console.WriteLine(wantedInt.Value);
             Console.WriteLine(wantdeDouble.Value);
+
+            Products p = new Products();
+            Console.WriteLine("오늘의 점심 메뉴는 " + p[2] + "입니다.");
+            p[2] = "단무지";
+            Console.WriteLine("오늘의 점심 메뉴는 " + p[2] + "입니다.");
         }
     }
 }
